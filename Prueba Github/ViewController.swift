bcf33de02622fc6a874695380fa07a9178edccad
Instituto Cultural Cuernavaca <ESTACION786@imac-de-instituto-2.local>
2015-11-07 12:05:40 -0600

    Initial Commit

---
+++ Prueba Github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba Github
+//
+//  Created by Instituto Cultural Cuernavaca on 07/11/15.
+//  Copyright © 2015 Instituto Cultural Cuernavaca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
